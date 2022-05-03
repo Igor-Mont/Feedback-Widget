0e02e6b8438e2ca9cf830f043342e98d02f25f20
Igor Mont <igornathandev@gmail.com>
2022-05-03 10:26:29 -0300

    feat: add screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft, Camera } from 'phosphor-react'
+import { ArrowLeft } from 'phosphor-react'
+import { useState } from 'react'
 import { FeedbackType, feedbackTypes } from '..'
 import { CloseButton } from '../../CloseButton'
+import { ScreenshotButton } from '../ScreenshotButton'
 
 type FeedbackContentStepProps = {
   feedbackType: FeedbackType;
@@ -8,6 +10,7 @@ type FeedbackContentStepProps = {
 }
 
 function FeedbackContentStep ({ feedbackType, onFeedbackRestartRequest }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
@@ -35,12 +38,10 @@ function FeedbackContentStep ({ feedbackType, onFeedbackRestartRequest }: Feedba
         />
 
         <footer className='flex gap-2 mt-2'>
-          <button
-            type='button'
-            className='p-2 bg-zinc-800 rounded-[4px] border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
-          >
-            <Camera className='w-6 h-6' />
-          </button>
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
 
           <button
             type="submit"
